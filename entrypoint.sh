99a116b2a71a4e84a65a3c770c5ae48a2b58b170
Zsolt Prontvai <prozsolt@gmail.com>
2020-03-09 15:57:27 +0000

    Fix things

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp /matchers.json .
-# echo "::add-matcher::matchers.json"
+echo "::add-matcher::matchers.json"
 rubocop --format emacs
-# echo "::remove-matcher owner=rubocop::"
+echo "::remove-matcher owner=rubocop::"
